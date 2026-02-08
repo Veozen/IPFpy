bade8f2c1cd0936a22d855cbd718aa0042414ac1
Veozen <152989587+Veozen@users.noreply.github.com>
2026-02-07 21:52:43 -0500

    Update test_ipf.py

--- tests/test_ipf.py
+++ tests/test_ipf.py
@@ -1,1 +1,37 @@
+"""
+Tests for IPFpy package
+"""
+
+import pytest
+from IPFpy import ipf, generate_random_table
+
+def test_ipf_basic_execution():
+    """
+    Tests that ipf produces a result for a random 4 dimensional table where each dimension has 8 categories
+    """
+    #step1 - create a table and generate the margins as well as the file that maps the cells of the inner table to the margins
+    raw_table = generate_random_table(4,8,scale=2)
+    input_table, margins, constraints = aggregate_table(raw_table, by=[0,1,2,3], var="value")
+    margins = margins.rename(columns={"value":"target"}) #rename margin column
+
+    # step2 - modify the margins by adding noise to the inner cells
+    new_table = input_table.copy().drop("unit_id",axis=1)
+    new_table["value"] =  input_table["value"] + np.random.uniform(-1, 1, input_table.shape[0])
+    modified_table, modified_margins, constraints = aggregate_table(new_table, by=[0,1,2,3], var="value")
+    modified_margins = modified_margins.rename(columns={"value":"target"})
+
+    # 2. Execution
+    # adjust the table in step1 to the margin obtained in step2
+    adjusted_table = ipf(   input=input_table,
+                            constraints=constraints,
+                            targets=modified_margins,
+                            unit_id="unit_id",
+                            var="value",
+                            cons_id="cons_id",
+                            db_file=None,
+                            tol=0.1,
+                            maxIter=1000)
+    # 3. Assertions
+    assert isinstance(adjusted_table, pd.DataFrame), f"Expected Pandas Dataframe, got {type(rounded)}"
+    assert len(adjusted_table) > 0, "The returned table is empty"
 
