21f69f4748deddcf9f15ae1ee641ac119bc6007e
Veozen <152989587+Veozen@users.noreply.github.com>
2026-02-07 22:27:12 -0500

    Update __init__.py

--- IPFpy/__init__.py
+++ IPFpy/__init__.py
@@ -1,5 +1,5 @@
-from .ipf import ipf, generate_random_table
+from .ipf import ipf, generate_random_table, aggregate_table
 
 __author__ = "Christian Gagné"
 __version__ = "0.1.0"
-__all__ = ["ipf", "generate_random_table"]
+__all__ = ["ipf", "generate_random_table", "aggregate_table"]
